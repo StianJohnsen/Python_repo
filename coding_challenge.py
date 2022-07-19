31ea1f4f1b09158a91df00f37a75bd94175cb1a7
StianJohnsen <stianjohnzz1@gmail.com>
2022-07-19 21:34:51 +0100

    Funker kun med et ord nå. Må finne ut av hva jeg skal gjøre med en return statement som kun utfører en gang

--- coding_challenge.py
+++ coding_challenge.py
@@ -1,14 +1,20 @@
 def check_duplicates(string):
     word_list = string.split()
-    print (word_list)
+    word_counter = -1
 
     for word in word_list:
+        word_counter += 1
+        letters_list = []
         for letters in word:
-            print (letters)
+            if letters.lower() not in letters_list:
+                letters_list.append(letters.lower())
+            else:
+                return (f'The word: {word_list[word_counter]} has duplicate letters')
+            
         
 
 
 
 
 #print (check_duplicates("Hello world"))
-check_duplicates("Hello world")
+print(check_duplicates("Lasse Stian Mariurs"))
